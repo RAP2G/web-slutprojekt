abcec6f1049967695f9a045db72b9dca19433349
Soma Szabó <szabosonka@gmail.com>
2022-04-05 08:22:01 +0200

    Created Basic Docker Environment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 
 const server = async ()=>{
 
-    const port = 5000; // default port to listen
+    const port = process.env.SERVER_PORT || 5000; // default port to listen
 
     // define a route handler for the default home page
     app.get( "/", ( req:any, res:any ) => {
